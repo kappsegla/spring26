5b3c89a2e3c66a639ad20fdadc4a2667f4c05bad
Martin Blomberg <martin.blomberg@outlook.com>
2026-03-05 08:56:47 +0100

    Prepare for form demo

--- src/main/java/org/example/spring26/books/BookController.java
+++ src/main/java/org/example/spring26/books/BookController.java
@@ -2,23 +2,12 @@ package org.example.spring26.books;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class BookController {
 
     private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
-    BookService bookService;
 
-    public BookController(BookService bookService) {
-        log.info("BookController constructor");
-        this.bookService = bookService;
-    }
-
-    @GetMapping("/books")
-    void getBook() {
-
-    }
 }
