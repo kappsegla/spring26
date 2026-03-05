981394284b897a3cc08264ec790acc548bb98e85
Martin Blomberg <martin.blomberg@outlook.com>
2026-03-05 09:48:39 +0100

    Add form handling for BookController and JTE templates

--- src/main/java/org/example/spring26/books/BookController.java
+++ src/main/java/org/example/spring26/books/BookController.java
@@ -3,11 +3,29 @@ package org.example.spring26.books;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
 
     private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
+    @GetMapping("/books/form")
+    public String showForm() {
+        return "form";
+    }
 
+    @PostMapping("/books/form")
+    public String processForm(
+            @RequestParam String name,
+            @RequestParam String email, Model model) {
+        //Validering
+        //Save to db?
+        model.addAttribute("name", name);
+        model.addAttribute("email", email);
+        return "result";
+    }
 }
