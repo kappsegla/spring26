a99478b21f7c03c9f73ace4f3aac941c282b740e
Martin Blomberg <martin.blomberg@outlook.com>
2026-03-10 09:41:41 +0100

    Add XSS endpoint with HTML escaping example in `XssController`

--- src/main/java/org/example/spring26/XssController.java
+++ src/main/java/org/example/spring26/XssController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class XssController {
@@ -18,5 +20,26 @@ public class XssController {
     public String protectedXss(@RequestParam(value = "name", defaultValue = "Guest") String name, Model model) {
         model.addAttribute("name", name);
         return "xss-protected";
+    }
+
+    @GetMapping("/xss")
+    @ResponseBody
+    public String xss() {
+        return """
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>test</title>
+                </head>
+                <body>
+                <h1>Home page</h1>
+                """ +
+                HtmlUtils.htmlEscape("<script>alert('Hej');</script>")
+                +
+                """
+                        </body>
+                        </html>
+                        """;
     }
 }
