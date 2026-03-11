c38a53cb868209fa4da5b5c497d3d674c221486b
Martin Blomberg <martin.blomberg@outlook.com>
2026-03-11 09:43:57 +0100

    Integrate Flyway for database migrations and update PostgreSQL version
    - Added Flyway dependencies in `pom.xml` for migration support.
    - Created `V1__InitialMigration.sql` for the initial schema setup.
    - Updated `docker-compose.yaml` to use PostgreSQL 17.
    - Changed `application.properties` to disable Hibernate auto-DDL with `spring.jpa.hibernate.ddl-auto=none`.
    - Removed demo data initialization from `Spring26Application`.

--- src/main/java/org/example/spring26/Spring26Application.java
+++ src/main/java/org/example/spring26/Spring26Application.java
@@ -1,12 +1,7 @@
 package org.example.spring26;
 
-import org.example.spring26.books.Book;
-import org.example.spring26.books.BookRepository;
-import org.example.spring26.books.BookService;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Spring26Application {
@@ -15,17 +10,17 @@ public class Spring26Application {
         SpringApplication.run(Spring26Application.class, args);
     }
 
-    @Bean
-    ApplicationRunner applicationRunner(BookRepository bookRepository) {
-        return args -> {
-            if( bookRepository.count() == 0 ) {
-                Book book = new Book();
-                book.setTitle("Book 1");
-                bookRepository.save(book);
-                book = new Book();
-                book.setTitle("Book 2");
-                bookRepository.save(book);
-            }
-        };
-    }
+//    @Bean
+//    ApplicationRunner applicationRunner(BookRepository bookRepository) {
+//        return args -> {
+//            if( bookRepository.count() == 0 ) {
+//                Book book = new Book();
+//                book.setTitle("Book 1");
+//                bookRepository.save(book);
+//                book = new Book();
+//                book.setTitle("Book 2");
+//                bookRepository.save(book);
+//            }
+//        };
+//    }
 }
