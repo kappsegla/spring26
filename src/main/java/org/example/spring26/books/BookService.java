fb97ae1398746920819481b77d981b6d883834fa
Martin Blomberg <martin.blomberg@outlook.com>
2026-04-29 08:57:58 +0200

    Remove HelloController, HelloServlet, ServletConfig, and related tests

--- src/main/java/org/example/spring26/books/BookService.java
+++ src/main/java/org/example/spring26/books/BookService.java
@@ -4,10 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
     private static final Logger log = LoggerFactory.getLogger(BookService.class);
-    public BookService() {
-        log.info("BookService constructor");
+
+
+    public List<Book> allBooks() {
+        return List.of(new Book("The Hobbit", 1), new Book("The Lord of the Rings", 2));
     }
 }
